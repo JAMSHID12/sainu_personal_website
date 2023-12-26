1fce9c379e50561364024b38922610e7fad2774f
Jamshi <jamshidct99@gmail.com>
2023-12-26 12:02:58 +0530

    site updated

--- assets/js/sainu.js
+++ assets/js/sainu.js
@@ -23,4 +23,32 @@ $('#nav-toggle').click(function(){
     $('ul.nav').toggleClass('show');
 });
 
+$(document).ready(function(){
+    $("#owl-carousel").owlCarousel({
+        items: 10, // Number of items to display
+        loop: true, // Infinite loop
+        margin: 10, // Space between items
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            },
+            1300:{
+                items:7
+            },
+            1600:{
+                items:9
+            },
+            1900:{
+                items:11
+            }
+        }
+    });
+});
+
 
